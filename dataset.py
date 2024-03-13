71463ae25d96992e11a0d001350b7135a08ca966
i4ata <ivaylo.russinov@gmail.com>
2024-03-13 15:15:31 +0100

    small updates

--- dataset.py
+++ dataset.py
@@ -4,20 +4,22 @@ import os
 import re
 from tqdm import tqdm
 
+import torch
+
 class DataCleaningPipeline:
 
     def __init__(self) -> None:
-        self.raw_text = self.load_raw_enwik8()
+        self.raw_text: str = None
+        self.cleaned_text: str = None
 
-    def load_clean_enwik8(self) -> str:
+    def load_clean_enwik8(self) -> None:
         """This function loads the cleaned text file from memory as a string"""
         path = 'data/enwik8_clean.txt'
         if not os.path.exists(path):
             self.clean_text()
         with open(path, encoding='utf-8') as f:
-            text = f.read()
+            self.cleaned_text = f.read()
         print('Cleaned text file successfully read from memory')
-        return text
 
     def clean_text(self) -> None:
         """This function performs basic cleaning of the raw data.
@@ -25,14 +27,13 @@ class DataCleaningPipeline:
         """
         path = 'data/enwik8_clean.txt'
         prefixes = {
-            '<', '==', '{', '#', ':', '|', '}', '&', '-', '!', '[[', ';', '__', '\\',
+            '<', '==', '{', '#', ':', '|', '}', '&', '-', '!', '[[', ';', '__', '\\', '*',
             'Image', '\'\'Main arcticle', 'Subclass', 'See also', '\'\'See also'
         }
         substrings_to_remove = (
             '&quot;', '&lt;', '&gt', r'http\S+', '&amp;nbsp;', '&amp;'
         )
         allowed_characters = r"[^a-z0-9 ]"
-        prefixes.add('*') # The * removes the bullet points, might be bad but most are useless
         with open(path, 'w', encoding='utf-8') as f:
             for line in tqdm(self.raw_text.split('\n')):
 
@@ -44,13 +45,14 @@ class DataCleaningPipeline:
                     or len(line) < 75 # to remove small lines, can change the number of needed
                 ):
                     continue
+                
                 line = line.lower()
                 for substr in substrings_to_remove:
                     line = re.sub(substr, "", line)
                 line = re.sub(allowed_characters, "", line)
                 f.write(line + '\n')
 
-    def load_raw_enwik8(self) -> str:
+    def load_raw_enwik8(self) -> None:
         """This function reads the enwik8 dataset in its raw format"""
         path = 'data/enwik8.txt'
         if not os.path.exists(path):
@@ -60,16 +62,26 @@ class DataCleaningPipeline:
                 for line in enwik8['train']:
                     f.write(line['text'] + '\n')
         with open(path, encoding='utf-8') as f:
-            text = f.read()
+            self.raw_text = f.read()
         print('Raw text file successfully read from memory')
-        return text
-    
-
+        
+    def text_to_tensor(self) -> torch.Tensor:
+        """This function will create the 2GB tensor, be careful to not destroy your pc.
+        I think we can lazily get the tensor when we create the dataset, so this is likely not needed
+        """
+        text = re.sub('\n', '', self.cleaned_text)
+        token_to_index = {token: i for i, token in enumerate(sorted(set(text)))}
+        one_hot_encoded = torch.zeros(len(text), len(token_to_index), dtype=torch.bool)
+        print(f'Total size: {(one_hot_encoded.element_size() * one_hot_encoded.numel()):_}')
+        for i in tqdm(range(len((text)))):
+            one_hot_encoded[i, token_to_index[text[i]]] = True
+        torch.save(one_hot_encoded, 'enwik8_onehot.pt')
+        return one_hot_encoded
     
 if __name__ == '__main__':
     dcp = DataCleaningPipeline()
-    length_original = len(dcp.raw_text)
-    cleaned = dcp.load_clean_enwik8()
-    length_cleaned = len(cleaned)
-    print(f'Original length: {length_original:_}, cleaned_length: {length_cleaned:_}')
-    print(f'Unique characters: {sorted(set(cleaned))}')
+    dcp.load_raw_enwik8()
+    dcp.load_clean_enwik8()
+    print(f'Original length: {len(dcp.raw_text):_}, cleaned_length: {len(dcp.cleaned_text):_}')
+    print(f'Unique characters: {"".join(sorted(set(dcp.cleaned_text)))}')
+    # dcp.text_to_tensor() #!!!
