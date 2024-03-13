4cc2e1515f102cfe2fecde48424095a2dabb669d
i4ata <ivaylo.russinov@gmail.com>
2024-03-13 13:57:00 +0100

    some basic cleaning done

--- dataset.py
+++ dataset.py
@@ -1,20 +1,75 @@
 from datasets import load_dataset
 
 import os
+import re
+from tqdm import tqdm
 
-def load_raw_enwik8() -> str:
-    """This function reads the enwik8 dataset in its raw format"""
-    path = 'data/enwik8.txt'
-    if not os.path.exists(path):
-        enwik8 = load_dataset('enwik8')
+class DataCleaningPipeline:
+
+    def __init__(self) -> None:
+        self.raw_text = self.load_raw_enwik8()
+
+    def load_clean_enwik8(self) -> str:
+        """This function loads the cleaned text file from memory as a string"""
+        path = 'data/enwik8_clean.txt'
+        if not os.path.exists(path):
+            self.clean_text()
+        with open(path, encoding='utf-8') as f:
+            text = f.read()
+        print('Cleaned text file successfully read from memory')
+        return text
+
+    def clean_text(self) -> None:
+        """This function performs basic cleaning of the raw data.
+        I remove punctuation, useless lines, xml expressions, etc.
+        """
+        path = 'data/enwik8_clean.txt'
+        prefixes = {
+            '<', '==', '{', '#', ':', '|', '}', '&', '-', '!', '[[', ';', '__', '\\',
+            'Image', '\'\'Main arcticle', 'Subclass', 'See also', '\'\'See also'
+        }
+        substrings_to_remove = (
+            '&quot;', '&lt;', '&gt', r'http\S+', '&amp;nbsp;', '&amp;'
+        )
+        allowed_characters = r"[^a-z0-9 ]"
+        prefixes.add('*') # The * removes the bullet points, might be bad but most are useless
         with open(path, 'w', encoding='utf-8') as f:
-            for line in enwik8['train']:
-                f.write(line['text'] + '\n')
-    with open(path) as f:
-        text = f.read()
-    return text
+            for line in tqdm(self.raw_text.split('\n')):
+
+                # remove lines that are potentially useless
+                if (
+                    line=='' 
+                    or any(line.startswith(prefix) for prefix in prefixes)
+                    or re.match(r"^''(.*)'':*$", line) is not None
+                    or len(line) < 75 # to remove small lines, can change the number of needed
+                ):
+                    continue
+                line = line.lower()
+                for substr in substrings_to_remove:
+                    line = re.sub(substr, "", line)
+                line = re.sub(allowed_characters, "", line)
+                f.write(line + '\n')
+
+    def load_raw_enwik8(self) -> str:
+        """This function reads the enwik8 dataset in its raw format"""
+        path = 'data/enwik8.txt'
+        if not os.path.exists(path):
+            enwik8 = load_dataset('enwik8')
+            os.mkdir(path.split('/')[0])
+            with open(path, 'w', encoding='utf-8') as f:
+                for line in enwik8['train']:
+                    f.write(line['text'] + '\n')
+        with open(path, encoding='utf-8') as f:
+            text = f.read()
+        print('Raw text file successfully read from memory')
+        return text
+    
+
     
 if __name__ == '__main__':
-    text = load_raw_enwik8()
-    number_of_lines = text.count('\n')
-    print(f'Number of lines: {number_of_lines}')
+    dcp = DataCleaningPipeline()
+    length_original = len(dcp.raw_text)
+    cleaned = dcp.load_clean_enwik8()
+    length_cleaned = len(cleaned)
+    print(f'Original length: {length_original:_}, cleaned_length: {length_cleaned:_}')
+    
