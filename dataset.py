737dc4b25e6de1a45abf4326fab42f209324a9d4
i4ata <ivaylo.russinov@gmail.com>
2024-04-13 20:43:39 +0200

    training aft-simple myb done

--- dataset.py
+++ dataset.py
@@ -1,14 +1,16 @@
 import torch
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader, random_split, Subset
 
 from typing import Tuple
 
 class EnWik8:
     """Main class for the dataset"""
 
-    def __init__(self, txt_dir: str = 'data/enwik8_clean.txt', sequence_length: int = 1024) -> None:
-        self.dataset = TorchDataset(txt_dir=txt_dir, sequence_length=sequence_length)
+    def __init__(self, txt_dir: str = 'data/enwik8_clean.txt', sequence_length: int = 1024, dataset_fraction: float = .001) -> None:
+        dataset = TorchDataset(txt_dir=txt_dir, sequence_length=sequence_length)
+        indices = torch.randperm(len(dataset))[:int(dataset_fraction * len(dataset))]
+        self.dataset = Subset(dataset=dataset, indices=indices)
 
     def split(self, lengths: Tuple[float, float, float] = (.8, .1, .1)) -> None:
         self.train_dataset, self.test_dataset, self.val_dataset = random_split(dataset=self.dataset, lengths=lengths)
@@ -18,20 +20,22 @@ class EnWik8:
         self.train_dataloader = DataLoader(
             dataset=self.train_dataset,
             batch_size=batch_size,
-            shuffle=True,
-            num_workers=num_workers
+            num_workers=num_workers,
+            pin_memory=True
         )
 
         self.val_dataloader = DataLoader(
             dataset=self.val_dataset,
             batch_size=batch_size,
-            num_workers=num_workers
+            num_workers=num_workers,
+            pin_memory=True
         )
 
         self.test_dataloader = DataLoader(
             dataset=self.test_dataset,
             batch_size=batch_size,
-            num_workers=num_workers
+            num_workers=num_workers,
+            pin_memory=True
         )
 
 class TorchDataset(Dataset):
--- train.py
+++ train.py
@@ -1,21 +1,24 @@
 import torch
 import torch.nn as nn
 from tqdm import tqdm
+import pickle 
 
 from dataset import EnWik8
 from transformer import DecoderOnlyAFT
+from early_stopper import EarlyStopper
 
 if __name__ == '__main__':
-
+    torch.manual_seed(0)
+    torch.cuda.manual_seed(0)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = DecoderOnlyAFT(
-        layers=12,
-        e_dim=256,
-        hid_dim=256,
+        layers=3,
+        e_dim=64,
+        hid_dim=64,
         vocab_size=37,
-        sequence_len=1024
+        sequence_len=256
     ).to(device)
-    enwik = EnWik8(sequence_length=1024)
+    enwik = EnWik8(sequence_length=256)
     enwik.split()
     enwik.get_dataloaders(batch_size=128)
     train_dataloader, val_dataloader = enwik.train_dataloader, enwik.val_dataloader
@@ -23,11 +26,23 @@ if __name__ == '__main__':
     print(f'Batches in val_dataloder: {len(val_dataloader)}')
     optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-3, weight_decay=.5)
     loss_fn = nn.CrossEntropyLoss()
+    early_stopper = EarlyStopper()
 
-    for epoch in range(100):
+    metrics = {
+        'train': {
+            'losses': [],
+            'accuracies': []
+        },
+        'val': {
+            'losses': [],
+            'accuracies': []
+        }
+    }
 
-        train_loss, val_loss = 0, 0
+    for epoch in tqdm(range(100)):
+        train_loss, val_loss, train_acc, val_acc = 0, 0, 0, 0
 
+        # Train loop
         model.train()
         for X, y in train_dataloader:
 
@@ -40,7 +55,9 @@ if __name__ == '__main__':
             optimizer.step()
 
             train_loss += loss.item()
+            train_acc += (y_pred.argmax(-1) == y).sum().item()
 
+        # Validation loop
         model.eval()
         for X, y in val_dataloader:
 
@@ -51,6 +68,43 @@ if __name__ == '__main__':
                 loss = loss_fn(y_pred, y)
 
             val_loss += loss.item()
+            val_acc += (y_pred.argmax(-1) == y).sum().item()
 
+        # Do early stopping
+        if early_stopper.check(val_loss):
+            print(f'Model stopped early due to risk of overfitting')
+            break
+
+        if early_stopper.save_model:
+            print('Saving model')
+            torch.save(model.state_dict(), 'models/model1.pt')
+
+        # Aggregate metrics
         train_loss, val_loss = train_loss / len(train_dataloader), val_loss / len(val_dataloader)
-        print(f'{epoch}: Train loss: {train_loss :.3}, Val loss: {val_loss :.3}')
+        train_acc, val_acc = train_acc / len(train_dataloader), val_acc / len(val_dataloader)
+        print(f'{epoch}: Train loss: {train_loss :.3}, Train acc: {train_acc :.3}, Val loss: {val_loss :.3}, Val acc: {val_acc :.3}')
+
+        # Register metrics
+        metrics['train']['losses'].append(train_loss)
+        metrics['train']['accuracies'].append(train_acc)
+        metrics['val']['losses'].append(val_loss)
+        metrics['val']['accuracies'].append(val_acc)
+
+    # Save the metrics
+    with open('metrics1.pkl', 'wb') as f:
+        pickle.dump(metrics, f)
+
+    # Test best model
+    model.load_state_dict(torch.load('models/model1.pt'))
+    test_loss, test_acc = 0, 0
+    for X, y in enwik.test_dataloader:
+
+        X, y = X.to(device), y.to(device)
+        
+        with torch.inference_mode():
+            y_pred = model(X)
+            loss = loss_fn(y_pred, y)
+
+        test_loss += loss.item()
+        test_acc += (y_pred.argmax(-1) == y).sum().item()
+    print(test_loss / len(enwik.test_dataloader), test_acc / len(enwik.test_dataloader))
